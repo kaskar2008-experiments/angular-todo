13f9bb81f4ce83865a95f752d24cd46c8bb03a47
Karen <kaskar2008@mail.ru>
2019-04-10 21:09:54 +0300

    Add [components/todo-item]: Single todo item

---
+++ src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.styl']
+})
+export class TodoItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
